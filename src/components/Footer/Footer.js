0fe07b6f77e62c477876f2c02ea7cad97708313b
HIMANSHU JAIN <64030123+himanshu03jain@users.noreply.github.com>
2021-11-04 01:45:23 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,9 +10,6 @@ function Footer() {
         <p className='footer-subscription-heading'>
           Subscribe to our Newsletter
         </p>
-        {/* <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p> */}
         <div className='input-areas'>
           <form>
             <input
@@ -64,80 +61,18 @@ function Footer() {
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
             <h2>About Us</h2>
-            {/* <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link> */}
           </div>
           <div class='footer-link-items'>
             <a href="https://www.entrepreneurshipnetwork.net/contact"><h2>FAQ's</h2></a>
-            {/* <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link> */}
+    
           </div>
           <div class='footer-link-items'>
             <a href="https://www.entrepreneurshipnetwork.net/contact"><h2>Contact Us</h2></a>
-            {/* <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link> */}
+            
           </div>
         </div>
       </div>
-      {/* <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='footer-logo'>
-            <Link to='/' className='social-logo'>
-              TRVL
-              <i class='fab fa-typo3' />
-            </Link>
-          </div>
-          <small class='website-rights'>TRVL © 2020</small>
-          <div class='social-icons'>
-            <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i class='fab fa-facebook-f' />
-            </Link>
-            <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i class='fab fa-instagram' />
-            </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i class='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i class='fab fa-twitter' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i class='fab fa-linkedin' />
-            </Link>
-          </div>
-        </div>
-      </section> */}
+     
       <div className="last">
         <h3 className="lastline">© 2020 Limitless Technologies - The Entrepreneurship Network</h3>
       </div>
